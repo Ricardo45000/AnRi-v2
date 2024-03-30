15ee523487885b8f080d8bcf1d80a719213ddc00
Ricardo45000 <ric4rdo.da.silva@gmail.com>
2024-03-30 23:37:45 +0000

    change gitignore

--- dist/out-tsc/environments/environment.prod.js
+++
@@ -1,8 +0,0 @@
-/**require('dotenv').config();
-console.log(process.env);**/
-export var environment = {
-    baseId: 'appMwUCNMxDixBpYd',
-    apiKey: 'patfeKY5ql4ehQ10U.46249ea71858e281581f337448030a35a5626d1d9ed4ccec8e0edd9d18a291d9',
-    production: true,
-};
-//# sourceMappingURL=environment.prod.js.map
