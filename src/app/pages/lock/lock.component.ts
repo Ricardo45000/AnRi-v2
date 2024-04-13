89b0eba95ad24c0160d9c39674bd8f9d3ae6876d
Ricardo45000 <ric4rdo.da.silva@gmail.com>
2024-04-13 19:36:29 +0100

    animations

--- src/app/pages/lock/lock.component.ts
+++ src/app/pages/lock/lock.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { AppComponent } from 'app/app.component';
 
@@ -9,7 +9,7 @@ import { AppComponent } from 'app/app.component';
     styleUrls: ['./lock.component.css']
 })
 
-export class LockComponent{
+export class LockComponent implements AfterViewInit{
     selectedLanguage: string;
 
     constructor(public translate: TranslateService, private root: AppComponent){
@@ -28,7 +28,22 @@ export class LockComponent{
     const e = (document.getElementById('mySelect') as HTMLInputElement).value
     this.selectedLanguage = e;
     this.root.switchLanguage(e);
-}
+  }
+
+  headerElements: NodeList;
+
+  ngAfterViewInit(): void {
+    this.headerElements = document.querySelectorAll('.logo');
+    this.fadeElements();
+  }
+
+  fadeElements() {
+    this.headerElements.forEach((element: HTMLElement, index: number) => {
+      setTimeout(() => {
+        element.style.opacity = '1';
+      }, index * 200); // Adjust delay by multiplying index with a value (in milliseconds)
+    });
+  }
     
     
 }
